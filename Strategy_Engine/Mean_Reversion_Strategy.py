d08331c9917da0ac65e64a2ff80d07a230dfd3fe
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-04-20 20:45:13 +0530

    Mean Reversion Strategy

---
+++ Strategy_Engine/Mean_Reversion_Strategy.py
@@ -0,0 +1,3 @@
+#Prices revert to average over time. This strategy is based on the idea that if a stock's price deviates significantly from its 
+# historical average, it will eventually revert back to that average.
+#Z = (Price - Rolling Mean) / Rolling Std Dev

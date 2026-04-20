fff13d2de4c49f661942d557b3537bee57a66a78
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-04-20 20:40:58 +0530

    momentum strategy based on the idea of price

---
+++ Strategy_Engine/Momentum_Strategy.py
@@ -0,0 +1,8 @@
+#stocks that went up continue going up, and stocks that went down continue going down. Momentum strategy is based on this idea 
+#of price persistence, where we buy assets that have shown strong performance in the past and sell assets that have shown 
+#weak performance.
+
+#Momentum Indicator: We can calculate a momentum indicator, such as the rate of change (ROC) or relative strength index (RSI), to 
+#identify overbought or oversold conditions. For example, if the RSI is above 70, it may indicate that the stock is overbought 
+#and could be due for a pullback, while an RSI below 30 may indicate that the stock is oversold and could be due for a rebound.
+# Return = Price(t) / Price(t-n)

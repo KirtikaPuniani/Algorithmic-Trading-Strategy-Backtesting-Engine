ecff372dd4bb16a5adb74d29915398159a5703c4
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-04-23 00:40:58 +0530

    updated code

--- Strategy_Engine/Momentum_Strategy.py
+++ Strategy_Engine/Momentum_Strategy.py
@@ -10,9 +10,32 @@
 import sys
 import os
 import pandas as pd
+import Data_Collection_Module.Data_Collection as dc
 
 data_path = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "..", "Data_Collection_Module")
 )             #adding data folder to path for imports
 
 sys.path.append(data_path)
+data = dc.collect_data()
+
+class MomentumStrategy:
+    def __init__(self, data, window=14):
+        self.data = data
+        self.window = window
+
+    def calculate_momentum(self):
+        self.data['Momentum'] = self.data['Close'] / self.data['Close'].shift(self.window)
+        return self.data
+
+    def generate_signals(self):
+        self.data['Signal'] = 0
+        self.data.loc[self.data['Momentum'] > 1, 'Signal'] = 1  # Buy signal
+        self.data.loc[self.data['Momentum'] < 1, 'Signal'] = -1 # Sell signal
+        return self.data   
+    
+if __name__ == "__main__":
+    momentum_strategy = MomentumStrategy(data)
+    momentum_strategy.calculate_momentum()
+    signals = momentum_strategy.generate_signals()
+    print(signals[['Close', 'Momentum', 'Signal']].tail())
